8a424e3f5b90556aea178aa38f9fb12c37754e9e
Your Name <you@example.com>
2024-01-05 15:20:43 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 using namespace std;
-
+template <typename T>
+void show(T d[],int N,int);
 template <typename T>
 void insertionSort(T d[],int N){
+for(int i = 1; i < N; i++){
 
+		T key = d[i];
+		int j = i-1;
+		while(j >= 0 && d[j] < key){
+			d[j+1] = d[j];
+			j--;
+		}
+		d[j+1] = key;
+	    show(d,N,i);
 }
-
+    
+}
+template <typename T>
+void show(T d[],int N,int i){
+    
+    cout << "Pass " << i << ":";
+    
+	for(int i = 0 ; i<N ; i++){
+		cout << d[i] << " ";
+	}
+	cout << endl;
+}
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
@@ -17,3 +38,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
